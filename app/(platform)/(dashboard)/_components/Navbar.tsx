74c9b30e2721349682e036b2f57660386397515b
lmacanda <laurina02@hotmail.com>
2024-08-18 14:26:06 +0100

    updated middleware

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -35,6 +35,7 @@ export const Navbar = () => {
             }}
           />
           <UserButton
+            afterSwitchSessionUrl="/"
             appearance={{
               elements: {
                 avatarBox: {
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,39 @@
+import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/forum(.*)",
+  "/organization(.*)",
+]);
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
+export default clerkMiddleware((auth, req) => {
+  const url = req.nextUrl;
+
+  // Case 1: Logged-in user accessing a public route
+  if (auth().userId && isPublicRoute(req)) {
+    let path = "/select-org";
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+
+    return NextResponse.redirect(new URL(path, url.origin));
   }
+
+  // Case 2: Unauthenticated user trying to access a protected route
+  if (!auth().userId && isProtectedRoute(req)) {
+    return NextResponse.redirect(new URL("/sign-in", url.origin));
+  }
+
+  // Case 3: Logged-in user without an organization ID trying to access non-org selection pages
+  if (auth().userId && !auth().orgId && url.pathname !== "/select-org") {
+    return NextResponse.redirect(new URL("/select-org", url.origin));
+  }
+
+  // If none of the conditions apply, continue with the request
+  return NextResponse.next();
 });
 
 export const config = {
